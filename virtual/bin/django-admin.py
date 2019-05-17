1fc151a56680ba0cfa33f4a9be9994a9e2338c09
Antavio <njuguna13@gmail.com>
2019-05-17 11:41:35 +0300

    Create the basic structure of the project.

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/antavio/Documents/Friday-Assignments/The_Gram/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

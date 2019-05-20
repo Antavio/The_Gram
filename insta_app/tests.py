9daefc21ec3da8bba87ae2f478722ebaceb4877c
Antavio <njuguna13@gmail.com>
2019-05-20 17:43:56 +0300

    Create tests for the Image Class Model.

--- insta_app/tests.py
+++ insta_app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Image,Profile
+from .models import Image,Profile,User
 
 # Create your tests here.
 
@@ -24,3 +24,21 @@ class ProfileTestClass(TestCase):
         profiles = Profile.filter_profile_by_id(self.new_profile.id)
         self.assertTrue(profiles == self.new_profile)
 
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = Profile(user_bio='Test Bio')
+        self.new_image = Image(id=2,img_title="Test Title",img_caption='Test Caption',date_posted='2017-02-01',insta_user=self.new_user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+    def test_save_instance(self):
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def test_delete_profile(self):
+        self.new_image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0)
